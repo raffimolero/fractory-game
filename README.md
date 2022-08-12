1525f5686f61ac8f929405f964c6071ed29ef3b0
raffimolero <raffimolero@gmail.com>
2022-08-12 23:10:51 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Fractory. A game you have never seen before...
+
+...mostly because it hasn't been made yet.
---
+++ src/main.rs
@@ -0,0 +1,5 @@
+// TODO: import and call loop from logic crate
+
+fn main() {
+    println!("Hello, world!");
+}
