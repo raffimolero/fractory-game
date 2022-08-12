a6961a9cb23b4a74645b87aa77e8fab327d5fd6b
mlokis <doka@stud.uniza.sk>
2022-08-12 19:25:10 +0200

    some attempts to make the renderer api

--- util/renderer/src/lib.rs
+++ util/renderer/src/lib.rs
@@ -1,8 +1,22 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+
+pub trait RenderTarget {
+    type Vertex: RenderVertex;
+    fn draw(&self, vertices: impl IntoIterator<Item = Self::Vertex>, indices: impl IntoIterator<Item = u32>);
+}
+
+pub trait RenderVertex: Sized {
+    const ASSERT: () = {
+        assert!(
+            std::mem::size_of::<Self>() % std::mem::size_of::<f32>() == 0,
+            "Vertex must be sequence of floating points"
+        );
+    };
+
+    const VERTEX_LAYOUT: &'static [usize];
+
+    fn to_raw_data(&self) -> [f32; std::mem::size_of::<Self>() / 4];
+}
+
+impl RenderVertex for u8 {
+    const VERTEX_LAYOUT: &'static [usize] = &[];
 }
