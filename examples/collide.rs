5857104791d0b3e512c1b460076ee22673bc4bce
Redstoneboi <raffimolero@gmail.com>
2023-02-08 22:06:42 +0800

    collision detection progress

--- examples/collide.rs
+++ examples/collide.rs
@@ -1,12 +1,21 @@
-use std::iter::repeat;
+use std::{iter::repeat, ops::Index};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-struct Item(i32);
+struct Item(usize);
+
+impl Item {
+    fn is_empty(self) -> bool {
+        self.0 == 0
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct Idx(usize);
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 struct Move {
-    from: usize,
-    to: usize,
+    from: Idx,
+    to: Idx,
 }
 
 #[derive(Debug, Clone)]
@@ -15,21 +24,53 @@ struct Moves {
 }
 
 impl Moves {
-    fn fix(&mut self) {
-        
-    }
+    fn fix(&mut self, things: &Things) {
+        #[derive(Debug, Clone, Copy)]
+        enum FromStatus {
+            Free,
+            Taken { motion_idx: usize, to: Idx },
+            Conflicted,
+        }
 
-    fn to_actions(self) -> Vec<Option<Item>> {
-        let mut v = vec![];
-        let mut append = |idx: usize, item: Item| {
-            if v.len() < idx {
-                let compensation = idx - v.len();
-                v.extend(repeat(None).take(compensation));
-                
+        let mut from_trackers = vec![FromStatus::Free; things.data.len()];
+        let mut to_trackers = vec![vec![]; things.data.len()];
+
+        let mut i = 0;
+        while i < self.motions.len() {
+            let Move { from, to } = self.motions[i];
+            if things[from].is_empty() {
+                self.motions.swap_remove(i);
+                continue;
             }
-        };
-        todo!()
+
+            let from_tracker = &mut from_trackers[from.0];
+            match from_tracker {
+                FromStatus::Free => *from_tracker = FromStatus::Taken(to),
+                FromStatus::Taken {
+                    motion_idx,
+                    to: arrow_to,
+                } => {
+                    from_trackers[from.0] = FromStatus::Conflicted;
+                    // TODO: figure out how to delete previous "from" item without messing up existing indices
+                    self.motions.swap(i, motion_idx);
+                    self.motions.swap_remove(i);
+                    continue;
+                }
+                FromStatus::Conflicted => {
+                    self.motions.swap_remove(i);
+                    continue;
+                }
+            }
+
+            to_ct[to.0] += 1;
+
+            i += 1;
+        }
     }
+}
+
+struct Actions {
+    actions: Vec<(Idx, Item)>,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -39,8 +80,16 @@ struct Things {
 
 impl Things {
     fn do_moves(&mut self, mut moves: Moves) {
-        moves.fix();
+        moves.fix(self);
         moves.to_actions();
+    }
+}
+
+impl Index<Idx> for Things {
+    type Output = Item;
+
+    fn index(&self, index: Idx) -> &Self::Output {
+        &self.data[index.0]
     }
 }
 
