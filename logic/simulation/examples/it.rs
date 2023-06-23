aaeea3478240a9e3cf5249b1a677f79a9b2c6c75
Redstoneboi <raffimolero@gmail.com>
2023-06-24 01:28:53 +0800

    restructure 2

--- logic/simulation/examples/it.rs
+++
@@ -1,7 +0,0 @@
-//! stands for Interactive Test.
-//! put whatever tests need to have a main function, especially with i/o.
-//!
-//! honestly could've just been a separate crate that imports this library,
-//! but i've found just using examples is easier when you don't want to write tests.
-
-fn main() {}
