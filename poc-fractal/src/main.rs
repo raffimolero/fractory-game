b13527c72d562e3445fc6be8d10cb6ef7c72ffe4
raffimolero <raffimolero@gmail.com>
2023-07-18 19:12:22 +0800

    mod tree, center rotation at mouse, lmb drag, +cleanup

--- poc-fractal/src/main.rs
+++ poc-fractal/src/main.rs
@@ -1,163 +1,14 @@
-use ::std::{
-    array,
-    f32::consts::TAU,
-    fmt::{Debug, Display},
-};
+mod tree;
 
+use crate::tree::QuadTree;
+use ::std::f32::consts::TAU;
+
+use ::ergoquad_2d::macroquad; // NOTE: ergoquad2d does not provide its own macro
 use ::ergoquad_2d::prelude::*;
-use ::rand::{distributions::Standard, prelude::*};
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_macro() {
-        let tre = tree! {
-            { 1, 2, 3, { 4, 5, ., 7} }
-        };
-        println!("{tre}");
-    }
-
-    #[test]
-    fn test_rand() {
-        let mut rng = thread_rng();
-        let tre = QuadTree::<u8>::rand(&mut rng, 5);
-        println!("{tre}");
-    }
-}
-
-enum QuadTree<T> {
-    Leaf(T),
-    Branch([Option<Box<Self>>; 4]),
-}
-
-impl<T> QuadTree<T> {
-    fn rand(rng: &mut impl Rng, depth: usize) -> Self
-    where
-        Standard: Distribution<T>,
-    {
-        let is_leaf = rng.gen_ratio(1, 1 << depth);
-        if is_leaf {
-            Self::Leaf(rng.gen())
-        } else {
-            Self::Branch(array::from_fn(|_| {
-                let is_none = rng.gen_ratio(1, 1 << depth);
-                (!is_none).then(|| Box::new(Self::rand(rng, depth - 1)))
-            }))
-        }
-    }
-}
-
-impl<T> QuadTree<T> {
-    fn _draw(&self, draw_leaf: &impl Fn(&T), depth: usize) {
-        let palette = [RED, ORANGE, YELLOW, GREEN, BLUE, PURPLE];
-        let col = palette[depth % palette.len()];
-
-        // draw base color
-        draw_rectangle(0.0, 0.0, 1.0, 1.0, col);
-
-        // draw outline
-        // let outline_thickness = 1.0 / 64.0;
-        // draw_rectangle_lines(0.0, 0.0, 1.0, 1.0, outline_thickness, BLACK);
-
-        let children = match self {
-            QuadTree::Leaf(val) => return draw_leaf(val),
-            QuadTree::Branch(children) => children,
-        };
-
-        // margin between child trees
-        let margin = 1.0 / 16.0;
-
-        let scale = upscale(0.5 - margin * 1.5);
-        for (y, row) in children.chunks_exact(2).enumerate() {
-            let y = y as f32 * (0.5 - margin / 2.0) + margin;
-            for (x, node) in row.iter().enumerate() {
-                let x = x as f32 * (0.5 - margin / 2.0) + margin;
-                if let Some(node) = node {
-                    apply(shift(x, y) * scale, || node._draw(draw_leaf, depth + 1));
-                }
-            }
-        }
-    }
-
-    fn draw(&self, draw_leaf: &impl Fn(&T)) {
-        self._draw(draw_leaf, 0);
-    }
-}
-
-impl<T: Display> Display for QuadTree<T> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            QuadTree::Leaf(val) => val.fmt(f),
-            QuadTree::Branch(children) => {
-                write!(f, "{{ ")?;
-                for child in children {
-                    match child {
-                        Some(val) => write!(f, "{val}")?,
-                        None => write!(f, ".")?,
-                    }
-                    write!(f, " ")?;
-                }
-                write!(f, "}}")
-            }
-        }
-    }
-}
-
-impl<T: Debug> Debug for QuadTree<T> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            QuadTree::Leaf(val) => val.fmt(f),
-            QuadTree::Branch(children) => {
-                write!(f, "{{ ")?;
-                for child in children {
-                    match child {
-                        Some(val) => write!(f, "{val:?}")?,
-                        None => write!(f, ".")?,
-                    }
-                    write!(f, " ")?;
-                }
-                write!(f, "}}")
-            }
-        }
-    }
-}
-
-/// builds a quadtree from braces, values, and dots
-/// ```
-/// let tree = tree! ({
-///     { . , (), (), .  },
-///     { (), (), . , () },
-///     { },
-///     .,
-/// });
-/// println!("{tree:?}");
-/// ````
-macro_rules! tree {
-    (node .) => {
-        None
-    };
-    (node $t:tt) => {
-        Some(Box::new(tree!($t)))
-    };
-
-    ({ $tl:tt,  $tr:tt, $bl:tt, $br:tt $(,)? }) => {
-        QuadTree::Branch([
-            tree!(node $tl),
-            tree!(node $tr),
-            tree!(node $bl),
-            tree!(node $br),
-        ])
-    };
-    ($t:expr) => {
-        QuadTree::Leaf($t)
-    };
-}
-pub(crate) use tree;
+use ::rand::prelude::*;
 
 /// returns a Mat4 corresponding to how much the map needs to be moved
-fn cam_control(mouse_prev: &mut Vec2) -> Mat4 {
+fn cam_control() -> Mat4 {
     let [mut x, mut y, mut rot] = [0.0; 3];
     let mut flipped = false;
     let mut zoom = 1.0;
@@ -170,20 +21,18 @@ fn cam_control(mouse_prev: &mut Vec2) -> Mat4 {
     // check mouse
     // mouse goes downwards, while transforms go upwards
     let mouse = mouse_position_local();
-    let mouse_delta = mouse - *mouse_prev;
+    // macroquad calculates delta position wrong, because macroquad
+    let mouse_delta = -mouse_delta_position();
 
     // scroll goes up, transforms zoom in
     let (_scroll_x, scroll_y) = mouse_wheel();
     {
         // zoom
         let scroll_sens = 1.0 / 60.0;
-        // println!("{scroll_y}");
         zoom *= (2_f32).powf(scroll_y * scroll_sens);
-        x -= mouse.x * (zoom - 1.0);
-        y -= mouse.y * (zoom - 1.0);
 
         // drag controls
-        if is_mouse_button_down(MouseButton::Right) {
+        if is_mouse_button_down(MouseButton::Left) {
             x += mouse_delta.x;
             y += mouse_delta.y;
         }
@@ -231,9 +80,7 @@ fn cam_control(mouse_prev: &mut Vec2) -> Mat4 {
         }
     }
 
-    *mouse_prev = mouse;
-
-    Mat4::from_scale_rotation_translation(
+    let main_transform = Mat4::from_scale_rotation_translation(
         Vec3 {
             x: if flipped { -zoom } else { zoom },
             y: zoom,
@@ -241,15 +88,15 @@ fn cam_control(mouse_prev: &mut Vec2) -> Mat4 {
         },
         Quat::from_rotation_z(rot),
         Vec3 { x, y, z: 0.0 },
-    )
-}
+    );
 
-// NOTE: ergoquad2d does not provide its own macro
-use ergoquad_2d::macroquad;
+    // center the transform at the mouse
+    shift(mouse.x, mouse.y) * main_transform * shift(-mouse.x, -mouse.y)
+}
 
 fn window_conf() -> Conf {
     Conf {
-        window_title: "WASD/Drag to move, Scroll to zoom, QE to rotate, RMB to flip.".to_owned(),
+        window_title: "WASD/Drag to move, Scroll to zoom, QE to rotate, F to flip.".to_owned(),
         window_width: 512,
         window_height: 512,
         fullscreen: false,
@@ -284,9 +131,6 @@ async fn main() {
     let cam = &mut Camera2D::default();
     cam.zoom = vec2(1.0, -1.0);
     set_camera(cam);
-
-    // mouse data
-    let mut mouse_prev = Vec2::default();
 
     // resource folder
     set_pc_assets_folder("../assets");
@@ -328,7 +172,7 @@ async fn main() {
             return;
         }
 
-        transform = cam_control(&mut mouse_prev) * transform;
+        transform = cam_control() * transform;
         apply(transform, || tree.draw(&draw_num(font, BLACK)));
 
         // end frame
---
+++ poc-fractal/src/tree.rs
@@ -0,0 +1,156 @@
+use ::std::{
+    array,
+    fmt::{Debug, Display},
+};
+
+use ::ergoquad_2d::prelude::*;
+use ::rand::{distributions::Standard, prelude::*};
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_macro() {
+        let tre = tree! {
+            { 1, 2, 3, { 4, 5, ., 7} }
+        };
+        println!("{tre}");
+    }
+
+    #[test]
+    fn test_rand() {
+        let mut rng = thread_rng();
+        let tre = QuadTree::<u8>::rand(&mut rng, 5);
+        println!("{tre}");
+    }
+}
+
+pub enum QuadTree<T> {
+    Leaf(T),
+    Branch([Option<Box<Self>>; 4]),
+}
+
+impl<T> QuadTree<T> {
+    pub fn rand(rng: &mut impl Rng, depth: usize) -> Self
+    where
+        Standard: Distribution<T>,
+    {
+        let is_leaf = rng.gen_ratio(1, 1 << depth);
+        if is_leaf {
+            Self::Leaf(rng.gen())
+        } else {
+            Self::Branch(array::from_fn(|_| {
+                let is_none = rng.gen_ratio(1, 1 << depth);
+                (!is_none).then(|| Box::new(Self::rand(rng, depth - 1)))
+            }))
+        }
+    }
+}
+
+impl<T> QuadTree<T> {
+    fn _draw(&self, draw_leaf: &impl Fn(&T), depth: usize) {
+        let palette = [RED, ORANGE, YELLOW, GREEN, BLUE, PURPLE];
+        let col = palette[depth % palette.len()];
+
+        // draw base color
+        draw_rectangle(0.0, 0.0, 1.0, 1.0, col);
+
+        // draw outline
+        // let outline_thickness = 1.0 / 64.0;
+        // draw_rectangle_lines(0.0, 0.0, 1.0, 1.0, outline_thickness, BLACK);
+
+        let children = match self {
+            QuadTree::Leaf(val) => return draw_leaf(val),
+            QuadTree::Branch(children) => children,
+        };
+
+        // margin between child trees
+        let margin = 1.0 / 16.0;
+
+        let scale = upscale(0.5 - margin * 1.5);
+        for (y, row) in children.chunks_exact(2).enumerate() {
+            let y = y as f32 * (0.5 - margin / 2.0) + margin;
+            for (x, node) in row.iter().enumerate() {
+                let x = x as f32 * (0.5 - margin / 2.0) + margin;
+                if let Some(node) = node {
+                    apply(shift(x, y) * scale, || node._draw(draw_leaf, depth + 1));
+                }
+            }
+        }
+    }
+
+    pub fn draw(&self, draw_leaf: &impl Fn(&T)) {
+        self._draw(draw_leaf, 0);
+    }
+}
+
+impl<T: Display> Display for QuadTree<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            QuadTree::Leaf(val) => val.fmt(f),
+            QuadTree::Branch(children) => {
+                write!(f, "{{ ")?;
+                for child in children {
+                    match child {
+                        Some(val) => write!(f, "{val}")?,
+                        None => write!(f, ".")?,
+                    }
+                    write!(f, " ")?;
+                }
+                write!(f, "}}")
+            }
+        }
+    }
+}
+
+impl<T: Debug> Debug for QuadTree<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            QuadTree::Leaf(val) => val.fmt(f),
+            QuadTree::Branch(children) => {
+                write!(f, "{{ ")?;
+                for child in children {
+                    match child {
+                        Some(val) => write!(f, "{val:?}")?,
+                        None => write!(f, ".")?,
+                    }
+                    write!(f, " ")?;
+                }
+                write!(f, "}}")
+            }
+        }
+    }
+}
+
+/// builds a quadtree from braces, values, and dots
+/// ```
+/// let tree = tree! ({
+///     { . , (), (), .  },
+///     { (), (), . , () },
+///     { },
+///     .,
+/// });
+/// println!("{tree:?}");
+/// ````
+macro_rules! tree {
+    (node .) => {
+        None
+    };
+    (node $t:tt) => {
+        Some(Box::new(tree!($t)))
+    };
+
+    ({ $tl:tt,  $tr:tt, $bl:tt, $br:tt $(,)? }) => {
+        QuadTree::Branch([
+            tree!(node $tl),
+            tree!(node $tr),
+            tree!(node $bl),
+            tree!(node $br),
+        ])
+    };
+    ($t:expr) => {
+        QuadTree::Leaf($t)
+    };
+}
+pub(crate) use tree;
