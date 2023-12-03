b69bd2511bd6d35246ebc1be83a5d2d05bb3baf5
raffimolero <raffimolero@gmail.com>
2023-12-03 13:41:49 +0800

    wip

--- src/data.rs
+++ src/data.rs
@@ -1,12 +1,53 @@
-/*
 use crate::logic::fractal::Behavior;
+
+// HACK
+type BitVec = Vec<bool>;
+
+pub struct FragmentData {
+    names: Vec<String>,
+    behaviors: Vec<Behavior>,
+}
 
 pub struct Planet {
     // icon: Icon,
     name: String,
     desc: String,
-    fragments: Vec<Fragment>,
+    fragments: FragmentData,
     biomes: Vec<Biome>,
+}
+
+impl Planet {
+    /// TODO: FOR TESTING PURPOSES
+    pub fn new_xyyy() -> Self {
+        let xyyy = [
+            ("", vec![]),
+            ("X", vec![]),
+            ("Y", vec![]),
+            ("Flip-Flop", flip_self_and_below_self()),
+            ("Spinner", hexagon()),
+            ("Rotor", rotate()),
+            ("Grower", grow()),
+            ("Sucker", suck()),
+            ("Wire", wire()),
+        ];
+        let frag_count = xyyy.len();
+        let mut names = Vec::with_capacity(frag_count);
+        let mut behaviors = Vec::with_capacity(frag_count);
+        for (name, behavior) in xyyy {
+            names.push(name.to_string());
+            behaviors.push(behavior);
+        }
+        Self {
+            name: "XYYY".into(),
+            desc: "The first planet.".into(),
+            fragments: FragmentData { names, behaviors },
+            biomes: vec![Biome {
+                name: "Landing Zone",
+                desc: "Contains every fragment.",
+                fragments: vec![true; frag_count],
+            }],
+        }
+    }
 }
 
 pub struct Biome {
@@ -22,4 +63,3 @@ pub struct Fragment {
     desc: String,
     behavior: Behavior,
 }
-*/
