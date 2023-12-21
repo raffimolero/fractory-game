af98f6d63979ab934969501fe8d4c0e642b042fe
raffimolero <raffimolero@gmail.com>
2023-12-21 23:30:24 +0800

    centered rotations on mouse, scale untested

--- poc-bevy/src/cam.rs
+++ poc-bevy/src/cam.rs
@@ -1,6 +1,9 @@
-use bevy::prelude::*;
+use bevy::{
+    prelude::*,
+    window::{PrimaryWindow, WindowResolution},
+};
 
-use crate::debug::WasdControl;
+use crate::debug::{control, WasdControl};
 
 #[derive(Component, Debug, Clone, Copy)]
 pub struct MainCam;
@@ -8,10 +11,52 @@ pub struct MainCam;
 pub struct Plug;
 impl Plugin for Plug {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, setup);
+        app.add_systems(Startup, setup).add_systems(
+            Update,
+            (
+                lock_to_mouse.before(control),
+                unlock_from_mouse.after(control),
+            ),
+        );
     }
 }
 
 fn setup(mut commands: Commands) {
     commands.spawn((Camera2dBundle::default(), MainCam, WasdControl));
+}
+
+fn cursor_to_px(mut cursor: Vec2, resolution: &WindowResolution) -> Vec2 {
+    let size = Vec2::new(resolution.width(), resolution.height());
+    cursor -= size / 2.0;
+    cursor.y *= -1.0;
+    cursor
+}
+
+fn window_cursor_px_centered(window: &Window) -> Option<Vec2> {
+    let cursor = window.cursor_position()?;
+    Some(cursor_to_px(cursor, &window.resolution))
+}
+
+fn lock_to_mouse(
+    window: Query<&Window, With<PrimaryWindow>>,
+    mut camera: Query<(&mut Transform, &Camera), With<MainCam>>,
+) {
+    let Some(cursor) = window_cursor_px_centered(window.single()) else {
+        return;
+    };
+    let (mut cam_tf, cam) = camera.single_mut();
+    let tf = cam_tf.rotation;
+    cam_tf.translation += tf * cursor.extend(0.0);
+}
+
+fn unlock_from_mouse(
+    window: Query<&Window, With<PrimaryWindow>>,
+    mut camera: Query<(&mut Transform, &Camera), With<MainCam>>,
+) {
+    let Some(cursor) = window_cursor_px_centered(window.single()) else {
+        return;
+    };
+    let (mut cam_tf, cam) = camera.single_mut();
+    let tf = cam_tf.rotation;
+    cam_tf.translation -= tf * cursor.extend(0.0);
 }
